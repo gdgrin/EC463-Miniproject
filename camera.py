bbb2a403d8357197da75ace0025fdcdb0662cce0
gdgrin <genedgrinberg@gmail.com>
2018-09-15 22:31:08 -0400

    adding camera script

---
+++ camera.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.resolution = (1920,1080)
+
+camera.framerate = 30
+camera.start_preview()
+camera.annotate_text = "Who wants to go eat"
+camera.start_recording('/home/pi/Documents/vid1.h264')
+
+sleep(5)
+
+
+
+
+# camera.capture('/home/pi/Documents/pic1.jpg')
+camera.stop_recording()
+camera.stop_preview()
